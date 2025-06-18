1ebd536c2b05080dacc759dab5bbcea990745228
Moji Shi <smoggy@sjtu.edu.cn>
2025-06-18 15:17:50 +0200

    update requirement

--- run_extract_ros.sh
+++ run_extract_ros.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # 激活conda环境但只使用PyTorch相关的包
 export PYTHONPATH=/home/smoggy/miniforge3/envs/r2d2_py38/lib/python3.8/site-packages:$PYTHONPATH
+export LD_PRELOAD=/home/smoggy/miniforge3/envs/r2d2_py38/lib/libtiff.so.5
 # 运行ROS节点
 python3 extract_ros.py
